868141e96e4e546d3aecf1b8be034b3c08e548f0
Sumit Kumar <sharansumitkumar@gmail.com>
2026-05-05 13:53:12 +0530

    first commit

---
+++ android/build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+// The Android Gradle Plugin builds the native code with the Android NDK.
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android") version "2.3.21"
+}
+
+group = "com.github.dart_lang.jni_flutter"
+version = "1.0"
+
+repositories {
+        google()
+        mavenCentral()
+    }
+
+android {
+    namespace 'com.github.dart_lang.jni_flutter'
+    compileSdk 37
+
+    defaultConfig {
+        minSdk = 24
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility JavaVersion.VERSION_21
+    }
+}
